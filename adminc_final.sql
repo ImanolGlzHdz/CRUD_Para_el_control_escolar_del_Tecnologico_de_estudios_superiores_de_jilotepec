f0b560b10ae1321bc6dd49138e5524d979bf1bf9
ImanolGlzHdz <56180089+ImanolGlzHdz@users.noreply.github.com>
2023-03-21 20:13:13 -0600

    Add files via upload
    CRUD de un sistema para la gestion y control de las calificaciones, horarios, inscripciones y reinscripciones en linea para el Tecnologio de Estudios Superiores de jilotepec con html5, css3, javascript, php y mysql

---
+++ adminc_final.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : conexionbd
+ Source Server Type    : MySQL
+ Source Server Version : 100425 (10.4.25-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : adminc
+
+ Target Server Type    : MySQL
+ Target Server Version : 100425 (10.4.25-MariaDB)
+ File Encoding         : 65001
+
+ Date: 21/03/2023 20:00:50
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
